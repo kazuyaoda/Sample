75030b75c51748bdbb54839eefb10f76e7eac1c9
Kazuyasky <kazuyasky@gmail.com>
2018-05-17 22:29:15 +0900

    Initial Commit
    create project and add pod to ignore

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by 小田和哉 on 2018/05/17.
+//  Copyright © 2018年 K.oda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
